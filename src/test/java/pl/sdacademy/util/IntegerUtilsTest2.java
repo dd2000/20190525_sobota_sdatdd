5f71b8d2986bc50cf16a261cc632283086e04669
Michal Bojanowski <bojanowski.michal89@gmail.com>
2019-05-24 13:59:26 +0200

    rename integer utils test

--- src/test/java/pl/sdacademy/util/IntegerUtilsTest2.java
+++ src/test/java/pl/sdacademy/util/IntegerUtilsTest2.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class IntegerUtilsTest {
+class IntegerUtilsTest2 {
 
     private IntegerUtils integerUtils = new IntegerUtils();
 
