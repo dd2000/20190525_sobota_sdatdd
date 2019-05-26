24ebfa97144b3cbf39ac1306e4a6f38688c3fe6b
Dariusz.Żulpo <dd2000@wp.pl>
2019-05-26 10:53:00 +0200

    Testowanie opr. niedziela 26 maja 2019 .  Pobierz repozytorium instruktora po zakończeniu zajęć - zrobione przykłady.

---
+++ src/test/java/pl/sdacademy/database/DatabaseConnectionTest.java
@@ -0,0 +1,26 @@
+package pl.sdacademy.database;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.*;
+// ćw_7/4 slajd 67
+class DatabaseConnectionTest {
+//    @Test
+//    void shouldThrowExceptionWhenRemovingDataAndDatabaseconnectionIsClosed(){
+//        //given
+//        DatabaseConnection databaseConnection = new DatabaseConnection();
+//        DatabaseStore databaseStore = new DatabaseStore();
+//        final String data = "Some data";
+//        databaseConnection.close();
+//        //when
+//
+//        //then
+//  ???      assertThatExceptionOfType(DatabaseConnection.class)
+//                .isThrownBy(()->databaseStore.removeData(data))
+//                .withMessage("Connection is not opened, Canot remove data");
+//
+//    } //shouldThrowExceptionWhenRemovingDataAndDatabaseconnectionIsClosed()
+//
+
+}
